105d5eddb6b3c78ee0306f3353806055a8beb747
Vlad Horobets <vlad_horob@ukr.net>
2026-09-23 16:28:20 +0300

    build: add base path for build

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   server: {
-    host: true
+    host: true,
   },
-  plugins: [
-    tailwindcss(),
-  ],
-})
+  plugins: [tailwindcss()],
+  base: "/soul-beauty/",
+});
